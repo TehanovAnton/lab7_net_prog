9b533f3982b458415fd82d156e716f0d578842c2
antony <tehanovanton@gmail.com>
2021-10-29 18:33:03 +0300

    server get all messages from clients

---
+++ ClienrMS/ClienrMS.cpp
@@ -0,0 +1,33 @@
+﻿#include <Windows.h>
+#include <string>
+#include <iostream>
+
+#define NAME_MS "\\\\.\\mailslot\\Slot"
+#define SIZE 100
+
+using namespace std;
+
+int main()
+{
+	try
+	{
+		LPCWSTR nameMailslot = TEXT(NAME_MS);
+		HANDLE hMailslot;
+		if ((hMailslot = CreateFile(nameMailslot, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL)) == INVALID_HANDLE_VALUE)
+			throw "CreatFile fail\n";
+
+		char outBuf[SIZE] = "Hello Server";
+		DWORD bytrsWritten;
+		if (!WriteFile(hMailslot, outBuf, sizeof(outBuf), &bytrsWritten, NULL))
+			throw "WriteFile fail\n";
+
+		CloseHandle(hMailslot);
+	}
+	catch (const char* error)
+	{
+
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ ServerMS.cpp
@@ -0,0 +1,43 @@
+﻿#include <Windows.h>
+#include <string>
+#include <iostream>
+
+#define NAME_MS "\\\\.\\mailslot\\Slot"
+#define SIZE 100
+
+using namespace std;
+
+int main()
+{    
+    try
+    {
+        LPCWSTR nameMailslot = TEXT(NAME_MS);
+        HANDLE hMailslot;            
+        if ((hMailslot = CreateMailslot(nameMailslot, NULL, MAILSLOT_WAIT_FOREVER, NULL)) == INVALID_HANDLE_VALUE)
+            throw "CreateMailslot fail\n";
+
+        cout << "Server start:" << endl;
+
+        while (true)
+        {
+            char inBuf[SIZE];
+            DWORD bytesRead;
+            if (!ReadFile(hMailslot, inBuf, sizeof(inBuf), &bytesRead, NULL))
+                throw "ReadFile fail";
+
+            cout << "Read message: " << inBuf << endl << endl;
+        }
+
+        cout << "Server stop" << endl << endl;
+
+        CloseHandle(hMailslot);
+
+    }
+    catch (const char* error)
+    {
+        cout << error;
+    }
+    
+    system("pause");
+    return 0;
+}
